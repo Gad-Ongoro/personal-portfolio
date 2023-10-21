120ac22fc5713a5d18f73be547ad6a480009cc4b
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-21 10:13:43 +0300

    mousemove

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //window.alert("Hello User");
+
+function windowCursor(){
+    let cursor1 = document.querySelector("div.cursor1");
+    let cursor2 = document.querySelector("div.cursor2");
+
+    document.addEventListener("mousemove", e => {
+        // cursor1.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+        cursor1.style.cssText = cursor2.style.cssText = `
+        top: ${e.clientY}px;
+        left: ${e.clientX}px;    
+        `;
+    });
+};
+windowCursor();
