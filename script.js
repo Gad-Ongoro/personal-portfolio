6faf50f299a3392acad4906271cd8a8458b03c45
Gad-Ongoro <gadongoro1@gmail.com>
2023-11-26 20:36:03 +0300

    Edited

--- script.js
+++ script.js
@@ -16,5 +16,29 @@ windowCursor();
 
 // carousel
 function certCarousel(){
+    let leftBtn = document.querySelector(".left-carousel-btn");
+    let rightBtn = document.querySelector(".right-carousel-btn")
+    let certTrack = document.querySelector(".cert")
     
+    leftBtn.addEventListener("click", (e)=>{
+        certTrack.style.cssText = `
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        `;
+
+        // certTrack.style.cssText = `
+        // transform: translate()
+        // `
+        console.log(certTrack);
+    })
+
+    rightBtn.addEventListener("click", (e)=>{
+        certTrack.style.cssText = `
+        transform: translate(-1000px, -50%);
+        `;
+        // console.log(rightBtn);
+    })
 };
+certCarousel();
